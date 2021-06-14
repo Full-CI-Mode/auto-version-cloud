fbb6135f292b37e9bb3c8195207eec693a05f49d
Mounir Missaoui <mmissaoui@mbazconsulting.com>
2021-06-14 16:04:35 +0100

    Beta Version

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var express = require('express');
 const createserver = require('./server')
 
 const app = createserver()
-app.listen(3005, ()=>{
+app.listen((process.env.PORT || 5000), ()=>{
   console.info("Server running on 3005 and awaiting files!!")
 })
